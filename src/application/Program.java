4fa4c1fe448fa6d813475bde1332c41cffd4e562
Victor Guerra <vvguerraa@gmail.com>
2023-03-22 16:46:11 -0300

    Starting to implement Board, Piece, Chess layer and printing the board

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,10 +1,21 @@
 package application;
 
+import java.util.Locale;
+import java.util.Scanner;
+
+import chess.ChessMatch;
+
 public class Program {
 
 	public static void main(String[] args) {
+		Locale.setDefault(Locale.US);
+		Scanner sc = new Scanner(System.in);
 		
-
+		ChessMatch chessMatch = new ChessMatch();
+		UI.printBoard(chessMatch.getPieces());
+		
+		
+		sc.close();
 	}
 
 }
